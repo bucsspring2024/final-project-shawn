380a879c37b0292e78e8f50139a3b5b77e0e6c1a
Shawn Healy <shealy30@live.com>
2024-04-13 19:29:04 -0400

    battle

--- src/ai.py
+++ src/ai.py
@@ -2,14 +2,21 @@ import random
 
 class AI:
     def __init__(self, board):
+        """
+        Initializes AI player, setting up their board and all possible moves.
+        Args: board (object): the board from Board class that the AI will be attacking.
+        Return: None
+        """
         self.board = board
         self.possible_moves = [(x, y) for x in range(board.size) for y in range(board.size)]
 
+    
     def make_move(self):
+        """
+        Randomly picks a cell for the AI to attack.
+        Args: None
+        Return: move (tuple) coordinates that the AI is attacking.
+        """
         move = random.choice(self.possible_moves)
         self.possible_moves.remove(move)
         return move
-
-    def update_possible_moves(self, move):
-        if move in self.possible_moves:
-            self.possible_moves.remove(move)
--- src/player.py
+++ src/player.py
@@ -1,8 +1,18 @@
-from board import Board
-
 class Player:
     def __init__(self, board):
+        """
+        Initializes board the user will be attacking.
+        Args: board (object): board from the Board class that the user is attacking.
+        Return: None
+        """
         self.board = board
 
+    
     def make_move(self, coord):
-        return self.board.receive_attack(coord)
+        """
+        Returns the coordinates that the player is attacking
+        Args: coord (tuple) coordinates of cell being attacked
+        Return: move (tuple) coordinates of cell being attacked
+        """
+        move = coord
+        return move
