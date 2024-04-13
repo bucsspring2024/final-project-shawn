6eaa398dddb5f4e13a370479c071b2017d34a43f
Shawn Healy <shealy30@live.com>
2024-04-13 18:15:39 -0400

    battle

--- src/cell.py
+++ src/cell.py
@@ -1,12 +1,29 @@
 class Cell:
     def __init__(self):
+        """
+        Initializes the cell object that will make the board. Each cell starts as not having a ship or being hit.
+        Args: None
+        Return: None
+        """
         self.is_ship = False
         self.is_hit = False
 
+    
     def place_ship(self):
+        """
+        Used in the add_ship method in Board class. Updates the cell so a ship is present.
+        Args: None
+        Return: None
+        """
         self.is_ship = True
 
+    
     def hit(self):
+        """
+        Used in the receive_attack method in Board class. First checks if the cell has already been hit and if not, updates it to hit and checks if ship is present.
+        Args: None
+        Return: True or False depending on if a cell that has a ship and has not been hit yet, is hit.
+        """
         if not self.is_hit:
             self.is_hit = True
             return self.is_ship
--- src/ship.py
+++ src/ship.py
@@ -1,12 +1,29 @@
 class Ship:
-    def __init__(self, name, size, coordinates):
-        self.name = name
+    def __init__(self, size, coordinates):
+        """
+        Initializes the ship object. Sets the size and coordinates of the ship, while starting at zero hits.
+        Args: size (int): the number of cells the ship occupies.
+        Args: coordinates (list of tuples): the coordinates of the cells that the ship occupies.
+        Return: None
+        """
         self.size = size
         self.coordinates = coordinates
         self.hits = 0
 
+    
     def is_sunk(self):
+        """
+        Checks if the ship is sunk.
+        Args: None
+        Return: True or False depending on if ship is sunk.
+        """
         return self.hits == self.size
 
+    
     def hit(self):
+        """
+        Adds a hit to the ship object
+        Args: None
+        Return: None
+        """
         self.hits += 1
