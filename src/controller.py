ac5268f312d4f3e60af5607270c15acbbecb0ee5
Shawn Healy <shealy30@live.com>
2024-04-19 18:55:09 -0400

    battle

--- src/controller.py
+++ src/controller.py
@@ -1,73 +1,70 @@
-import pygame
-import pygame_menu
-import pygame_gui
+from board import Board
 from player import Player
 from ai import Ai
-from board import Board
-
-
-
 
 class Controller:
-    def __init__(self, player, ai):
-        pygame.init()
-        self.screen = pygame.display.set_mode()
-        self.background = pygame.Surface(pygame.display.get_window_size())
-        self.background.fill((150, 150, 250))
-
-        width, height = pygame.display.get_window_size()
-        self.menu = pygame_menu.Menu("button example", width-20, height/2, position = (10,10))
-        self.menu.add.label("Press for a Hero", max_char = -1, font_size = 14)
-
-        self.menu.add.button(
-            'Press Me', 
-            self.start_game, 
-            align=pygame_menu.locals.ALIGN_CENTER
-        )
-        
-        self.button = Button(
-            x=50,
-            y=self.menu.get_rect().bottom + 10
-        )
-
-        self.player = player
-        self.ai = ai
+    def __init__(self):
+        self.player_board = Board()
+        self.ai_board = Board()
+        self.player = Player(self.player_board)
+        self.ai = Ai(self.ai_board)
         self.current_turn = "player"
 
+
+        self.state = "menu"
     
-    def set_player_ships(self):
+
+    def mainloop(self):
+        while True:
+            if self.state == "menu":
+                self.menu_loop()
+            elif self.state == "game":
+                self.game_loop()
+            else:
+                self.end_loop()
+    
+    
+
+    def menu_loop(self):
         pass
 
-    def set_ai_ships(self):
+
+
+    def game_loop(self):
+        pass
+
+    
+    def end_loop(self):
         pass
 
 
-    def switch_turns(self):
-        self.current_turn = "ai" if self.current_turn == "player" else "player"
+
+    def game_setup(self):
+        self.set_player_ships()
+        self.set_ai_ships()
 
     
     def play_round(self):
         if self.current_turn == "player":
-            coord = self.player.make_move()
+            coord = self.player.make_move() #Add input from pygame
             hit = self.ai.board.receive_attack(coord)
         else:
             coord = self.ai.make_move()
             hit = self.player.board.receive_attack(coord)
         self.switch_turns()
 
-    
-    def get_player_input(self):
-        pass
+    def switch_turns(self):
+        self.current_turn = "ai" if self.current_turn == "player" else "player"
 
     def check_game_over(self):
         if self.player.board.all_sunk():
             return True
         elif self.ai.board.all_sunk():
             return True
-        
+    
+    def set_player_ships(self):
+        pass
 
-    def start_game(self):
-        game_over = False
-        while not game_over:
-            self.play_round()
-            game_over = self.check_game_over()
+    def set_ai_ship(self):
+        for _ in range(5):
+            
